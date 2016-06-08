877acd4923f20e862857b03cb6a2951f52ae6f72
Benjamin Dauer <dauer.benjamin@gmail.com>
2016-06-08 09:52:37 +0000

    fix login

--- twitter_clone/main.py
+++ twitter_clone/main.py
@@ -14,7 +14,20 @@ def connect_db(db_name):
 
 @app.before_request
 def before_request():
+    '''
+    Open a database connection before each request
+    '''
     g.db = connect_db(app.config['DATABASE'][1])
+
+
+@app.teardown_request
+def teardown_request(exception):
+    '''
+    Close the database connection after each request if one exists
+    '''
+    db = getattr(g,'db',None)
+    if db is not None:
+        db.close()
 
 
 def login_required(f):
@@ -25,34 +38,65 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
-@app.route("/login", methods=['POST', 'GET'])
+
+# implement your views here
+@app.route("/")
+def home():
+    return redirect(url_for('login'))
+    
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    error = None
+    '''
     if request.method == 'GET':
         return render_template('login.html')
-
+    '''
     if request.method == 'POST':
         # check if username in database, not get all users
         # request.form is dict with all the data from POST
         # request.form[user], request.form[password]
-
-        cur = g.db.cursor()
-        cur.execute\
-        ('SELECT username, password FROM user' +
+        cur = g.db.execute('SELECT username, password FROM user WHERE username=?'\
+        ,(request.form['username'],))
+        user = cur.fetchone()
+        if user:
+            if request.form['password'] != user[1]:
+                error = 'Invalid password'
+            else:
+                session['username'] = user[0]
+                return redirect(url_for('own_feed'))
+                #return render_template('own_feed.html')
+        else:
+            error = 'Invalid username'
+        return error
+    return render_template('login.html')
+        
+        
+    '''
+        cur = g.db.execute\
+        ('SELECT username, password FROM user'\
         'WHERE username={0}, password={1}'.\
         format(request.form['username'], request.form['password']))
-        user = cur.fetchone()
-        '''
-        cursor = g.db.execute + \
-        ('SELECT username, password FROM user' +
-        'WHERE username={0}, password={1}'. \
-        format(request.form['username'], request.form['password']))
         user = cursor.fetchone()
-        '''
+        
+        
         if user:
             return url_for('profile.html', user=user)
+    '''
+
+@app.route('/logout') #add variable username
+@login_required
+def logout():
+    pass
+
+@app.route('/own_feed') #add variable username
+@login_required
+def own_feed():
+    
+    pass
+    
 
 
-# implement your views here
-@app.route("/")
-def main():
-    return render_template('profile.html')
+
+
+
