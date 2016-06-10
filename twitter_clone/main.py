8ee200cbc4e413a21c2ba484bf9722297b1f2c23
Philipp Horn <philipp.horn4@gmail.com>
2016-06-10 19:43:22 +0000

    initial commit

--- twitter_clone/main.py
+++ twitter_clone/main.py
@@ -8,7 +8,6 @@ from flask import (g, request, session, redirect, render_template,
 
 app = Flask(__name__)
 
-
 def connect_db(db_name):
     return sqlite3.connect(db_name)
 
@@ -33,7 +32,7 @@ def feed():
     # if the user is NOT logged in: redirect to /login
     # if the user IS logged in, redirect to: /<username>
    
-    return redirect(url_for('user_profile', user=session['username']))
+    return redirect(url_for('user_tweets', user=session['username']))
     
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -47,7 +46,7 @@ def login():
     elif request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        password = md5(password).hexdigest()
+        password = md5(password.encode('utf-8')).hexdigest()
 
         cursor = g.db.execute("SELECT id, username, password FROM user")
         entries = [dict(user_id=row[0], name=row[1], password=row[2]) for row in cursor.fetchall()]
@@ -72,32 +71,34 @@ def logout():
     
 
 @app.route('/<user>', methods = ['POST', 'GET'])
-def user_profile(user):
+def user_tweets(user):
 
     if request.method == 'POST' :
         if "username" in session and session["username"] == user:
             tweet_content = request.form['tweet']
             user_id = session["user_id"]
-            g.db.execute("INSERT INTO tweet (user_id, content) VALUES (:user_id , :content)", {"user_id" : user_id, "content" : tweet_content})
-            g.db.commit()
+            
+            if len(tweet_content) <= 140:
+                g.db.execute("INSERT INTO tweet (user_id, content) VALUES (:user_id , :content)", {"user_id" : user_id, "content" : tweet_content})
+                g.db.commit()
         else:
             abort(403)
         
-    content = get_user_content(user)
+    content = get_tweet_content(user)
+    own_tweets = False
+    username = session["username"] if "username" in session else ""
     
     if content is None:
         return "User does not exist"
     elif "username" in session and session["username"] == user:
         own_tweets=True
         username = session["username"]
-    else:
-        username = ""
-        own_tweets=False
+ 
         
     return render_template("static_templates/own_feed.html", tweets=content, own_tweets=own_tweets, username=username, logged_in = "username" in session)
     
-
-def get_user_content(user):
+    
+def get_tweet_content(user):
     cursor = g.db.execute("SELECT id, username FROM user WHERE username = (?)", (user,))
     lst = cursor.fetchall()
     
@@ -105,8 +106,44 @@ def get_user_content(user):
         return None
         
     user_id = lst[0][0]
-    cursor = g.db.execute("SELECT created, content FROM tweet WHERE user_id = (?)", (user_id,))
-    content = [dict(created=row[0], content=row[1]) for row in cursor.fetchall()]
+    cursor = g.db.execute("SELECT created, content, id FROM tweet WHERE user_id = (?) ORDER BY created desc", (user_id,))
+    content = [dict(created=row[0], content=row[1], tweet_id=row[2]) for row in cursor.fetchall()]
     
     return content 
      
+     
+@app.route('/profile', methods = ['POST', 'GET'])
+@login_required
+def user_profile():
+    username = session['username']
+    user_id = session['user_id']
+    
+    if request.method == 'POST' :
+        data = ( request.form['first_name'], request.form['last_name'], request.form['birth_date'], user_id )
+
+        g.db.execute("UPDATE user SET first_name=?, last_name=?, birth_date =? WHERE user.id=?", data)
+        g.db.commit()
+    
+    cursor = g.db.execute("SELECT first_name, last_name, birth_date FROM user WHERE id = (?)", (user_id,))
+    first_name, last_name, birth_date = tuple(cursor.fetchall()[0])
+    
+
+    return render_template("static_templates/profile.html", first_name = first_name, last_name = last_name, birth_date = birth_date, username = username)
+    
+@app.route('/tweets/<tweet_id>/delete', methods = ['POST'])
+def delete_tweet(tweet_id): 
+  
+    if not str(tweet_id).isdigit():
+        abort(404)
+    
+    if "username" not in session:
+        return redirect("login")
+    
+    g.db.execute("DELETE FROM tweet WHERE id=?", (tweet_id,))
+    g.db.commit()
+    
+    return redirect('/')
+    
+    
+    
+    
