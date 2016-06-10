0dc0fd400db11c886d51de772a24e32b48c04def
helen <thehelenzhang@gmail.com>
2016-06-10 23:11:19 +0000

    in progress

--- twitter-schema.sql
+++ twitter-schema.sql
@@ -28,9 +28,11 @@ CREATE TABLE tweet (
   )
 );
 
---INSERT INTO "user" ("id", "username", "password", "birth_date") VALUES (10, "martinzugnoni", "81dc9bdb52d04dc20036dbd8313ed055", "2016-01-26");
---INSERT INTO "user" ("id", "username", "password", "birth_date") VALUES (11, "ivanzugnoni", "81dc9bdb52d04dc20036dbd8313ed055", "2016-07-06");
---INSERT INTO "tweet" ("user_id", "content") VALUES (10, "Hello World!");
---INSERT INTO "tweet" ("user_id", "content") VALUES (10, "This is so awesome");
---INSERT INTO "tweet" ("user_id", "content") VALUES (10, "Testing twitter clone");
---SELECT * FROM tweet;
+--I changed martinzugnoni's password to cookies, to be able to access his account
+INSERT INTO "user" ("id", "username", "password", "birth_date") VALUES (10, "martinzugnoni", "55e7dd3016ce4ac57b9a0f56af12f7c2", "2016-01-26");
+INSERT INTO "user" ("id", "username", "password", "birth_date") VALUES (14, "luis", '55e7dd3016ce4ac57b9a0f56af12f7c2', "2016-06-23");
+INSERT INTO "user" ("id", "username", "password", "birth_date") VALUES (11, "ivanzugnoni", "81dc9bdb52d04dc20036dbd8313ed055", "2016-07-06");
+INSERT INTO "tweet" ("user_id", "content") VALUES (10, "Hello World!");
+INSERT INTO "tweet" ("user_id", "content") VALUES (10, "This is so awesome");
+INSERT INTO "tweet" ("user_id", "content") VALUES (10, "Testing twitter clone");
+SELECT * FROM tweet;
