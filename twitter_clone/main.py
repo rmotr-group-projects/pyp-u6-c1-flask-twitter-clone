7c0aa652ef6fb6323ed7397e396948fba8aa0814
Philipp Horn <philipp.horn4@gmail.com>
2016-06-11 12:54:32 +0000

    restructure

--- twitter_clone/main.py
+++ twitter_clone/main.py
@@ -32,7 +32,7 @@ def feed():
     # if the user is NOT logged in: redirect to /login
     # if the user IS logged in, redirect to: /<username>
    
-    return redirect(url_for('user_tweets', user=session['username']))
+    return redirect(url_for('show_tweets', user=session['username']))
     
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -57,9 +57,11 @@ def login():
             else:
                 session['user_id'] = dictionary['user_id']
                 session['username'] = username
+                flash("You have been logged in")
                 
                 return redirect(next_url)
         return render_template("static_templates/login.html", error=error)
+        
         
 @app.route('/logout')
 @login_required
@@ -70,20 +72,24 @@ def logout():
     return redirect(url_for('feed'))
     
 
-@app.route('/<user>', methods = ['POST', 'GET'])
-def user_tweets(user):
-
-    if request.method == 'POST' :
-        if "username" in session and session["username"] == user:
-            tweet_content = request.form['tweet']
-            user_id = session["user_id"]
-            
-            if len(tweet_content) <= 140:
-                g.db.execute("INSERT INTO tweet (user_id, content) VALUES (:user_id , :content)", {"user_id" : user_id, "content" : tweet_content})
-                g.db.commit()
-        else:
-            abort(403)
+@app.route('/<user>', methods = ['POST'])
+def post_tweet(user):
+    if "username" in session and session["username"] == user:
+        tweet_content = request.form['tweet']
+        user_id = session["user_id"]
         
+        if len(tweet_content) <= 140:
+            g.db.execute("INSERT INTO tweet (user_id, content) VALUES (:user_id , :content)", {"user_id" : user_id, 
+                                                                                               "content" : tweet_content})
+            g.db.commit()
+            flash("Tweet posted")
+            
+        return show_tweets(user)
+    else:
+        abort(403)
+
+@app.route('/<user>', methods = ['GET'])
+def show_tweets(user):
     content = get_tweet_content(user)
     own_tweets = False
     username = session["username"] if "username" in session else ""
@@ -93,9 +99,50 @@ def user_tweets(user):
     elif "username" in session and session["username"] == user:
         own_tweets=True
         username = session["username"]
- 
         
-    return render_template("static_templates/own_feed.html", tweets=content, own_tweets=own_tweets, username=username, logged_in = "username" in session)
+    return render_template("static_templates/own_feed.html", tweets=content, 
+                                                             own_tweets=own_tweets, 
+                                                             username=username, 
+                                                             logged_in = "username" in session)
+    
+     
+@app.route('/profile', methods = ['POST', 'GET'])
+@login_required
+def user_profile():
+    username = session['username']
+    user_id = session['user_id']
+    
+    if request.method == 'POST' :
+        data = ( request.form['first_name'], request.form['last_name'], request.form['birth_date'], user_id )
+
+        g.db.execute("UPDATE user SET first_name=?, last_name=?, birth_date =? WHERE user.id=?", data)
+        g.db.commit()
+        flash("Profile Updated")
+        
+    
+    cursor = g.db.execute("SELECT first_name, last_name, birth_date FROM user WHERE id = (?)", (user_id,))
+    first_name, last_name, birth_date = tuple(cursor.fetchall()[0])
+    
+
+    return render_template("static_templates/profile.html", first_name = first_name, 
+                                                            last_name = last_name, 
+                                                            birth_date = birth_date, 
+                                                            username = username)
+    
+@app.route('/tweets/<tweet_id>/delete', methods = ['POST'])
+def delete_tweet(tweet_id): 
+  
+    if not str(tweet_id).isdigit():
+        abort(404)
+    
+    if "username" not in session:
+        return redirect("login")
+    
+    g.db.execute("DELETE FROM tweet WHERE id=?", (tweet_id,))
+    g.db.commit()
+    flash("Tweet deleted")
+    
+    return redirect('/')
     
     
 def get_tweet_content(user):
@@ -111,39 +158,3 @@ def get_tweet_content(user):
     
     return content 
      
-     
-@app.route('/profile', methods = ['POST', 'GET'])
-@login_required
-def user_profile():
-    username = session['username']
-    user_id = session['user_id']
-    
-    if request.method == 'POST' :
-        data = ( request.form['first_name'], request.form['last_name'], request.form['birth_date'], user_id )
-
-        g.db.execute("UPDATE user SET first_name=?, last_name=?, birth_date =? WHERE user.id=?", data)
-        g.db.commit()
-    
-    cursor = g.db.execute("SELECT first_name, last_name, birth_date FROM user WHERE id = (?)", (user_id,))
-    first_name, last_name, birth_date = tuple(cursor.fetchall()[0])
-    
-
-    return render_template("static_templates/profile.html", first_name = first_name, last_name = last_name, birth_date = birth_date, username = username)
-    
-@app.route('/tweets/<tweet_id>/delete', methods = ['POST'])
-def delete_tweet(tweet_id): 
-  
-    if not str(tweet_id).isdigit():
-        abort(404)
-    
-    if "username" not in session:
-        return redirect("login")
-    
-    g.db.execute("DELETE FROM tweet WHERE id=?", (tweet_id,))
-    g.db.commit()
-    
-    return redirect('/')
-    
-    
-    
-    
