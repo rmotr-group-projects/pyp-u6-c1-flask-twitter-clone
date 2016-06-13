a0cdeb33e687b7b03f00c2723fb1f63761584a1b
Benjamin Dauer <dauer.benjamin@gmail.com>
2016-06-13 20:06:53 +0000

    All functionality done

--- twitter_clone/main.py
+++ twitter_clone/main.py
@@ -92,24 +92,14 @@ def logout():
 #     if session['username'] == username:
         
 
-@app.route('/<username>')
-@login_required
+@app.route('/<username>', methods=['POST', 'GET'])
 def tweets(username):
-    # prepared_statement = """
-    # select id, user_id, created, content from tweet where
-    # user_id='{}'
-    # """.format(session['user_id'])
-    # cur = g.db.execute('select id, user_id, created, content from tweet where user_id=?',(session['user_id'],))
-    # cur = g.db.execute('select tweet.id, tweet.user_id, tweet.created, tweet.content from tweet join user on tweet.user_id = user.id')
-    #cur = g.db.execute('select id from user where username=?',(username,))
-    #user_id = cur.fetchone()[0]
-    cur = g.db.execute('select created, content from tweet where user_id=?', (get_user_id(username),))
-    tweets = [dict(created = row[0], content = row[1]) for row in cur.fetchall()]
-    # js = json.dumps(tweets)
-    # return js
-    if username != session['username']:
-        # cur = g.db.execute('SELECT content FROM tweet INNER JOIN user ON'\
-        # 'tweet.user_id = user.id')
+    if request.method == 'POST':
+        tweet = request.form['tweet']
+        insert_tweet(username, tweet)
+    cur = g.db.execute('select id, created, content from tweet where user_id=? order by id desc', (get_user_id(username),))
+    tweets = [dict(tweet_id = row[0], created = row[1], content = row[2]) for row in cur.fetchall()]
+    if 'username' not in session or username != session['username']:
         return render_template('other_feed.html', tweets = tweets, username = username)
     return render_template('own_feed.html', tweets = tweets, username = username)
 
@@ -117,14 +107,13 @@ def tweets(username):
 @app.route('/profile', methods=['POST', 'GET'])
 @login_required
 def profile():
-    # data = {
-    #     'id':get_user_id(username),
-    #     'first_name':get_first_name(username),
-    #     'last_name':get_last_name(username),
-    #     'birth_date':get_birth_date(username)
-    # }
-    pass
-    # return render_template('profile.html', data=data)
+    data = {
+        'id':session['user_id'],
+        'first_name':session['first_name'],
+        'last_name':session['last_name'],
+        'birth_date':session['birth_date']
+    }
+    return render_template('profile.html', data=data)
 
 
 def get_first_name(username):
@@ -159,23 +148,24 @@ def get_user_id(username):
     select id from user where username="{}"
     """.format(username)
     return g.db.execute(prepared_statement).fetchone()[0]
+
+def insert_tweet(username, tweet):
+    """method to insert the tweet for the username"""
+    user_id = get_user_id(username)
+    prepared_statement = """
+    insert into tweet (`user_id`, `content`) values ({}, '{}')
+    """.format(user_id, tweet)
+    g.db.execute(prepared_statement)
+    g.db.commit()
    
    
 @app.route('/tweets/<username>', methods=['POST', 'GET'])
 @login_required
 def own_feed(username):
-    
-    if request.method == 'GET':
-        prepared_statement = "SELECT id, user_id, created, content from tweet where user_id='{}'".format(get_user_id(username))
-        cur = g.db.execute(prepared_statement)
-        tweets = [dict(tweet_id=row[0], name=username, date=row[2], content=row[3]) for row in cur.fetchall()]
-        return render_template("own_feed.html", tweets=tweets)
-        
-    if request.method == 'POST':
-        prepared_statement = """INSERT INTO 'tweet' ('user_id', 'content') VALUES ({}, {})""".format(username, request.form['tweet'])
-        cur = g.db.execute(prepared_statement)
-        return redirect(url_for('own_feed'))
-        
+    prepared_statement = "SELECT id, user_id, created, content from tweet where user_id='{}'".format(get_user_id(username))
+    cur = g.db.execute(prepared_statement)
+    tweets = [dict(tweet_id=row[0], name=username, date=row[2], content=row[3]) for row in cur.fetchall()]
+    return render_template("own_feed.html", tweets=tweets)
 
 
 @app.route('/other_feed')
@@ -183,14 +173,19 @@ def own_feed(username):
 def other_feed():
     render_template('other_feed.html')
 
-@app.route('/tweets/<tweet_id>/delete', methods=['POST'])
-@login_required
+
+@app.route('/tweets/<int:tweet_id>/delete', methods=['POST'])
+@login_required #not necessary
 def delete_tweet(tweet_id):
-    cur = g.db.execute('SELECT * from tweet where user_id=?', (session['user_id'],))
-    if len(cur.fetchall()) < 2:
-        flash("Sorry, you must have at least two tweets in order to delete one.")
-        return redirect(url_for('own_feed'))
-    
-    cur = g.db.execute('DELETE FROM tweet WHERE id=?',(request.form['tweet-id'],))
-    flash("Tweet deleted successfully.")
-    return redirect(url_for('own_feed'))
+    # cur = g.db.execute('SELECT * from tweet where user_id=?', (session['user_id'],))
+    # if len(cur.fetchall()) < 2:
+    #     flash("Sorry, you must have at least two tweets in order to delete one.")
+    #     return redirect(url_for('own_feed')
+    # cur = g.db.execute('DELETE FROM tweet WHERE id=?',(request.form['tweet-id'],))
+    g.db.execute('DELETE FROM tweet WHERE id=?',(tweet_id,))
+    g.db.commit()
+    # cur = g.db.execute("SELECT id FROM tweet WHERE id=?",(tweet_id,))
+    # tweet_id = cur.fetchone()
+    flash("Tweet deleted successfully")
+    # return str(tweet_id)
+    return redirect(request.script_root)
