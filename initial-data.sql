e1e6fb53fafcd0eb3172ee775f120796f3f27990
adoniasdantas <adoniasdantas@gmail.com>
2016-09-14 17:32:35 -0300

    passing 15 tests, 2 missing

--- initial-data.sql
+++ initial-data.sql
@@ -2,8 +2,15 @@
 
 --INSERT INTO "user" ("id", "username", "password", "birth_date") VALUES (10, "martinzugnoni", "81dc9bdb52d04dc20036dbd8313ed055", "2016-01-26");
 --INSERT INTO "user" ("id", "username", "password", "birth_date") VALUES (11, "ivanzugnoni", "81dc9bdb52d04dc20036dbd8313ed055", "2016-07-06");
--- INSERT INTO "user" ("id", "username", "password", "birth_date") VALUES (13, "kpk1", "group3", "2016-09-13");
-INSERT INTO "user" ("id", "username", "password", "birth_date") VALUES (14, "kpk1", "group3", "2016-09-13");
---INSERT INTO "tweet" ("user_id", "content") VALUES (10, "Hello World!");
---INSERT INTO "tweet" ("user_id", "content") VALUES (10, "This is so awesome");
---INSERT INTO "tweet" ("user_id", "content") VALUES (10, "Testing twitter clone");
+-- INSERT INTO "user" ("id", "username", "password", "birth_date") VALUES (13, "adoniasdantas", "group3", "2016-09-13");
+-- INSERT INTO "user" ("id", "username", "password", "birth_date") VALUES (14, "kpk1", "group3", "2016-09-13");
+-- INSERT INTO "tweet" ("user_id", "content") VALUES (13, "Hello World!");
+-- INSERT INTO "tweet" ("user_id", "content") VALUES (13, "This is so awesome");
+-- INSERT INTO "tweet" ("user_id", "content") VALUES (14, "Testing twitter clone");
+-- INSERT INTO "tweet" ("user_id", "content") VALUES (14, "Python is so awesome");
+
+INSERT INTO "user" ("id", "username", "password", "birth_date") VALUES (15, "adonias", "78733d0dd44e9bbca1d931c569676531", "2016-09-13");
+INSERT INTO "tweet" ("user_id", "content") VALUES (15, "Hello World!");
+INSERT INTO "tweet" ("user_id", "content") VALUES (15, "This is so awesome");
+INSERT INTO "tweet" ("user_id", "content") VALUES (15, "Testing twitter clone");
+INSERT INTO "tweet" ("user_id", "content") VALUES (15, "Python is so awesome");
--- twitter_clone/main.py
+++ twitter_clone/main.py
@@ -33,20 +33,21 @@ def login_required(f):
 def hello_world():
     #return "Welcome to Twitter Clone"
     if 'username' in session:
-        return redirect(url_for('own_feed',username = session['username']))
+        return redirect(url_for('own_feed', username=session['username']))
     else:
         return redirect(url_for('login'))
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if 'username' in session:
-        return redirect('http://localhost/') #not sure if this is correct implementation
+        return redirect('/') #not sure if this is correct implementation
     error = None
     if request.method == 'POST':
         
         # Pull query to grab usernames and password from db
         cursor = g.db.execute("""
-            SELECT username,  password, id
+            SELECT username, password, id, first_name, last_name, birth_date
             FROM user;
             """)
         
@@ -54,7 +55,7 @@ def login():
         users = {}
         for row in cursor.fetchall():
             # print(row)
-            users[row[0]] = (row[1], row[2])
+            users[row[0]] = (row[1], row[2], row[3], row[4], row[5])
         username = request.form['username']
         # For clarity but may result in errors if username not in users
         # password = users[username][0]
@@ -66,7 +67,7 @@ def login():
 
         # Check validity of username and password, if valid, redirect to feed
         if username not in users:
-            error = 'Invalid username or password' # Changed to pass test (JLZ) 
+            error = 'Invalid username or password' # Changed to pass test (JLZ)
             # error = 'Invalid username'
         elif md5(request.form['password']).hexdigest() != users[username][0]:
             error = 'Invalid username or password' # Changed to pass test (JLZ)
@@ -74,29 +75,89 @@ def login():
             session['logged_in'] = True
             session['username'] = username
             session['user_id'] = users[username][1]
+            fields = ('password', 'id', 'first_name', 'last_name', 'birth_date')
+            session['user'] = dict(zip(fields, users[username]))
             return redirect(url_for('own_feed', method='GET', username=username))
     return render_template('static_templates/login.html', error=error)
 
 
 @app.route('/<username>', methods=['GET', 'POST'])
-@login_required
 def own_feed(username):
     if request.method == 'GET':
-        
+        #if someone is logged in
+        if 'username' in session:
+            #The own username feed
+            if username == session['username']:
+                cursor = g.db.execute("""
+                    SELECT a.id as user_id, a.username as username, c.id as tweet_id, c.content as tweet_content, c.created as tweet_time
+                    FROM user a INNER JOIN tweet c ON a.id = c.user_id ORDER BY c.created DESC;
+                """)
+                data = cursor.fetchall()
+
+                # It would be more efficient to use WHERE in the SQL query in order to
+                # only pull tweets that match username. For sake of time, we move on.
+                tweets = []
+                for row in data:
+                    if row[1] == username:
+                        tweets.append(row)
+
+                return render_template('static_templates/own_feed.html', username=username, tweets=tweets, session=session)
+
+            #Other users's feed
+            else:
+
+                cursor = g.db.execute("""
+                    SELECT a.id as user_id, a.username as username, c.id as tweet_id, c.content as tweet_content, c.created as tweet_time
+                    FROM user a INNER JOIN tweet c ON a.id = c.user_id;
+                """)
+
+                data = cursor.fetchall()
+
+                # It would be more efficient to use WHERE in the SQL query in order to
+                # only pull tweets that match username. For sake of time, we move on.
+                tweets = []
+                for row in data:
+                    if row[1] == username:
+                        tweets.append(row)
+                return render_template('static_templates/other_feed.html', session=session, username=username, tweets=tweets)
+        #Nobody is logged in
+        else:
+            cursor = g.db.execute("""
+                SELECT a.id as user_id, a.username as username, c.id as tweet_id, c.content as tweet_content, c.created as tweet_time
+                FROM user a INNER JOIN tweet c ON a.id = c.user_id;
+            """)
+            data = cursor.fetchall()
+
+            # It would be more efficient to use WHERE in the SQL query in order to
+            # only pull tweets that match username. For sake of time, we move on.
+            tweets = []
+            for row in data:
+                if row[1] == username:
+                    tweets.append(row)
+            return render_template('static_templates/other_feed.html', session=session, tweets=tweets)
+
+    #request.method == 'POST'
+    else:
+        new_tweet_body = request.form['tweet']
+        sql_statement = 'INSERT INTO "tweet" ("user_id", "content") VALUES (?, ?);'
+
+        g.db.execute(sql_statement, (session['user_id'], new_tweet_body, ))
+        g.db.commit()
+
         cursor = g.db.execute("""
             SELECT a.id as user_id, a.username as username, c.id as tweet_id, c.content as tweet_content, c.created as tweet_time
-            FROM user a INNER JOIN tweet c ON a.id = c.user_id;
+            FROM user a INNER JOIN tweet c ON a.id = c.user_id ORDER BY c.created DESC;
         """)
         data = cursor.fetchall()
-        
+
         # It would be more efficient to use WHERE in the SQL query in order to
         # only pull tweets that match username. For sake of time, we move on.
         tweets = []
         for row in data:
             if row[1] == username:
                 tweets.append(row)
+        return render_template('static_templates/own_feed.html', username=username, tweets=tweets)
 
-        return render_template('static_templates/own_feed.html', username=username, data=tweets)
 
 @app.route('/logout')
 @login_required
@@ -109,13 +170,33 @@ def logout():
     session.pop('logged_in', None)
     session.pop('user_id', None)
     session.pop('username', None)
-    return redirect('http://localhost/') #not sure if this is correct implementation
+    session.pop('first_name', None)
+    session.pop('last_name', None)
+    session.pop('birth_date', None)
+    session.pop('user', None)
+    return redirect('/') #not sure if this is correct implementation
 
 
-@app.route('/tweets/<tweet_id>/delete')
+@app.route('/tweets/<tweet_id>/delete', methods=['GET', 'POST'])
 @login_required
-def delete_tweet(tweet_id, username):
-    sql_statement = "DELETE FROM tweet WHERE id={}".format(tweet_id)
-    cursor = g.database.execute(sql_statement)
-    return redirect(url_for('own_feed', username=username))
-    
+def delete_tweet(tweet_id):
+    sql_statement = "DELETE FROM tweet WHERE id = ?"
+    cursor = g.db.execute(sql_statement, (tweet_id, ))
+    g.db.commit()
+    # return redirect(url_for('own_feed', username=session['username']))
+    return redirect('http://localhost/')
+
+@app.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile_page():
+    if request.method == 'POST':
+        sql_statement = "UPDATE user SET first_name = ?, last_name = ?, birth_date = ? WHERE id = ?"
+        cursor = g.db.execute(sql_statement, (request.form['first_name'], request.form['last_name'],
+                                              request.form['birth_date'], session['user_id'], ))
+        g.db.commit()
+        cursor = g.db.execute("SELECT first_name, last_name, birth_date FROM user WHERE id = ?", (session['user_id'], ))
+
+        fields = ('first_name', 'last_name', 'birth_date')
+        session['user'] = dict(zip(fields, cursor.fetchall()[0]))
+
+    return render_template('static_templates/profile.html')
