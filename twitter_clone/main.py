d1f7e04693bef4d423fbec4b695caccfe1d41289
Adonias Dantas <adoniasdantas@gmail.com>
2016-09-14 05:53:55 +0000

    Pradeep's changes, all authentication test cases passing, total 8/17 passing

--- twitter_clone/main.py
+++ twitter_clone/main.py
@@ -31,10 +31,16 @@ def login_required(f):
 @app.route('/')
 @login_required
 def hello_world():
-    return "Welcome to Twitter Clone"
+    #return "Welcome to Twitter Clone"
+    if 'username' in session:
+        return redirect(url_for('own_feed',username = session['username']))
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if 'username' in session:
+        return redirect('http://localhost/') #not sure if this is correct implementation
     error = None
     if request.method == 'POST':
         
@@ -49,7 +55,6 @@ def login():
         for row in cursor.fetchall():
             # print(row)
             users[row[0]] = (row[1], row[2])
-            
         username = request.form['username']
         # For clarity but may result in errors if username not in users
         # password = users[username][0]
@@ -63,13 +68,12 @@ def login():
         if username not in users:
             error = 'Invalid username or password' # Changed to pass test (JLZ) 
             # error = 'Invalid username'
-        elif request.form['password'] != users[username][0]:
+        elif md5(request.form['password']).hexdigest() != users[username][0]:
             error = 'Invalid username or password' # Changed to pass test (JLZ)
         else:
             session['logged_in'] = True
             session['username'] = username
             session['user_id'] = users[username][1]
-            # flash('You were logged in')
             return redirect(url_for('own_feed', method='GET', username=username))
     return render_template('static_templates/login.html', error=error)
 
@@ -99,7 +103,10 @@ def own_feed(username):
 def logout():
     # session.pop('logged_in', None)
     # flash('You were logged out')
-    session['logged_in'] = False
-    session['user_id'] = None
-    session['username'] = None
-    return redirect(url_for('login'))
+    #session['logged_in'] = False
+    #session['user_id'] = None
+    #session['username'] = None
+    session.pop('logged_in', None)
+    session.pop('user_id', None)
+    session.pop('username', None)
+    return redirect('http://localhost/') #not sure if this is correct implementation
