2b73bd1bb605d481bd3a0a4f796002960c2c4fa6
Stephan Gouin <sgouin@gmail.com>
2016-11-09 03:08:20 +0000

    tests pass on python 2

--- twitter_clone/main.py
+++ twitter_clone/main.py
@@ -2,7 +2,7 @@ import sqlite3
 from hashlib import md5
 from functools import wraps
 from flask import Flask
-from flask import (g, request, session, redirect, render_template,
+from flask import (abort, g, request, session, redirect, render_template,
                    flash, url_for)
 
 app = Flask(__name__)
@@ -27,3 +27,115 @@ def login_required(f):
 
 
 # implement your views here
+def query_db(query, args=(), one=False):
+    cur = g.db.execute(query, args)
+    rv = [dict((cur.description[idx][0], value)
+               for idx, value in enumerate(row)) for row in cur.fetchall()]
+    return (rv[0] if rv else None) if one else rv
+    
+    
+@app.route('/<username>', methods=['POST', 'GET'])
+def feed(username):
+    if request.method == 'POST':
+        if not session.get('user_id', None):
+            abort(403)
+            return redirect('/', 403)
+        
+        query_db('insert into tweet (content, user_id) values (?, ?)',
+               [request.form['tweet'], session.get('user_id')])
+        
+        g.db.commit()
+        
+        flash('New entry was successfully posted')
+    
+    tweets = query_db('''
+    SELECT t.*, u.username FROM tweet t INNER JOIN user u 
+            ON t.user_id = u.id 
+            WHERE username=? ORDER BY t.created DESC''', [username])
+    
+    if username == session.get('username'):
+        return render_template('static_templates/own_feed.html', tweets=tweets)
+    else:
+        return render_template('static_templates/other_feed.html', tweets=tweets)
+
+
+@app.route('/')
+@login_required
+def home():
+    return redirect(url_for('feed', username=session.get('username')))
+
+@app.route('/tweets/<int:id>/delete', methods=['POST'])
+@login_required
+def delete_tweet(id):
+    query_db('DELETE FROM tweet WHERE id=?', [id])
+    g.db.commit()
+    return redirect('/')
+
+'''
+curl -X POST -d '{"tweet":"xyz"}' http://twitter-clone-stefgou.c9users.io/martinzugnoni
+'''
+
+ 
+@app.route('/profile', methods=['POST', 'GET'])
+@login_required
+def profile():
+
+    
+    if request.method == 'POST':
+        
+        #query db for users == user
+        username = request.form['username']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        birth_date = request.form['birth_date']
+        
+
+        query_db('''UPDATE user
+                SET username = ?, first_name= ?, last_name = ?, birth_date = ?
+                WHERE id = ?''',
+                [username, first_name, last_name, birth_date, session.get('user_id')])
+        
+        g.db.commit()
+    
+    
+    user = query_db('SELECT * FROM user WHERE id = ?',
+                [session.get('user_id')], one=True)
+                
+    return render_template('static_templates/profile.html', user=user)
+    
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+            
+    if session.get('username'):
+        return redirect('/')
+    
+    if request.method == 'POST':
+        #query db for users == user
+        username = request.form['username']
+        password = md5(request.form['password']).hexdigest()
+
+        user = query_db('SELECT * FROM user WHERE username = ? and password = ?',
+                [username, password], one=True)
+
+        if not user:
+            error='Invalid username or password'
+            return render_template('static_templates/login.html', error=error)
+        else:
+            session['username'] = username
+            session['user_id'] = user['id']
+            flash('You were logged in')
+            return redirect('/')
+
+        
+    return render_template('static_templates/login.html')
+
+
+@app.route('/logout')
+def logout():
+    if session.get('username', None):
+        session.pop('username')
+    if session.get('user_id', None):
+        session.pop('user_id')
+    
+    return redirect('/')
