d806b8c091a4a46ed39018d6978a45ececcdb96a
David Lonoff <david.lonoff+pyp@gmail.com>
2017-05-23 00:52:10 +0000

    Clean up code a bit, get tox passing

--- twitter_clone/main.py
+++ twitter_clone/main.py
@@ -5,8 +5,6 @@ from functools import wraps
 from flask import Flask
 from flask import (g, request, session, redirect, render_template,
                    flash, url_for)
-if sys.version_info >= (3,):
-    unicode = str
 
 app = Flask(__name__)
 
@@ -24,44 +22,41 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'username' not in session:
-            return redirect(url_for('login_page', next=request.url))
+            return redirect(url_for('login', next=request.url))
         return f(*args, **kwargs)
     return decorated_function
 
 
 @app.route('/')
+@login_required
 def index():
-    if 'username' not in session:
-        return redirect(url_for('login_page'), code=302)
-    else:
-        return render_template('static_templates/own_feed.html')
+    if 'username' in session:
+        return redirect('/{}'.format(session['username']))
 
 @app.route('/logout')
 def logout_page():
     session.pop('username', None)
     session.pop('user_id', None)
-    return redirect(url_for('index'))
+    return redirect('/')
 
 @app.route('/login', methods=['POST', 'GET'])
-def login_page():
+def login():
     if request.method == 'GET':
         if 'username' in session:
-            return redirect(url_for('index'), code=302)
+            return redirect('/')
         else:
-            return render_template('static_templates/login.html', code=200)
+            return render_template('login.html', code=200)
     elif request.method == 'POST':
-        user_name = request.form['username'].encode('utf-8')
-        hash_pass = md5(str(request.form['password']).encode('utf-8')).hexdigest()
+        user_name = request.form['username']
+        hash_pass = md5(request.form['password'].encode('utf-8')).hexdigest()
 
-        cursor = g.db.execute('SELECT id, password FROM user WHERE username=?;', (user_name, ))
-        results = cursor.fetchall()
-        if results:
-            if hash_pass != results[0][1]:
-                return "Invalid username or password.", 200
-            else:
-                session['username'] = user_name
-                session['user_id'] = results[0][0]
-                return redirect(url_for('user_page', user_name=user_name), code=302)
+        query = 'SELECT id, password FROM user WHERE username=?;'
+        cursor = g.db.execute(query, (user_name, ))
+        user_match = cursor.fetchone()
+        if user_match and user_match[1] == hash_pass:
+            session['username'] = user_name
+            session['user_id'] = user_match[0]
+            return redirect('/{}'.format(user_name))
         else:
             return "Invalid username or password.", 200
 
@@ -73,14 +68,14 @@ def user_page(user_name):
         tweets = [dict(id=row[0], created=row[1], content=row[2], username=row[3]) for row in cursor.fetchall()]
         if 'username' in session:
             if session['username'] == user_name:
-                return render_template('static_templates/own_feed.html', code=200, user=user_name, tweets=tweets)
-            return render_template('static_templates/other_feed.html', code=200, user=session['username'], tweets=tweets)
+                return render_template('own_feed.html', code=200, user=user_name, tweets=tweets)
+            return render_template('other_feed.html', code=200, user=session['username'], tweets=tweets)
         else:
-            return render_template('static_templates/other_feed.html', code=200, user='', tweets=tweets)
+            return render_template('other_feed.html', code=200, user='', tweets=tweets)
     elif request.method == 'POST':
         if 'username' in session:
             if session['username'] == user_name:
-                new_tweet = request.form['tweet'].encode('utf-8')
+                new_tweet = request.form['tweet']
                 cursor = g.db.execute('SELECT id FROM user WHERE username=?;', (user_name, ))
                 user_id = cursor.fetchone()[0]
                 g.db.execute('INSERT INTO tweet (user_id, content) VALUES (?, ?);', (user_id, new_tweet))
@@ -89,7 +84,7 @@ def user_page(user_name):
                 cursor = g.db.execute('SELECT tweet.id, tweet.created, tweet.content, user.username from tweet '
                                       'INNER JOIN user ON tweet.user_id=user.id AND user.username=?;', (user_name,))
                 tweets = [dict(id=row[0], created=row[1], content=row[2], username=row[3]) for row in cursor.fetchall()]
-                return render_template('static_templates/own_feed.html', code=200, user=user_name, tweets=tweets)
+                return render_template('own_feed.html', code=200, user=user_name, tweets=tweets)
             return 'Not your username', 403
         else:
             return 'Not logged in', 403
@@ -101,11 +96,11 @@ def get_profile():
         cursor = g.db.execute('SELECT * FROM user WHERE username=?;', (user_name,))
         result = cursor.fetchone()
         user_id, username, password, first_name, last_name, birth_date = result
-        return render_template('static_templates/profile.html',
+        return render_template('profile.html',
                                user=username, first_name=first_name,
                                last_name=last_name, birth_date=birth_date, code=200)
     else:
-        return redirect(url_for('login_page'), code=302)
+        return redirect(url_for('login'))
 
 @app.route('/profile', methods=['POST'])
 def post_profile():
@@ -128,7 +123,7 @@ def delete_tweet(tweet_num):
     if result is None:
         return 'Invalid tweet number', 404
     if 'username' not in session:
-        return redirect(url_for('login_page'), code=302)
+        return redirect(url_for('login'))
     else:
         # tweet is valid, get the user_id and username to check that person has authorization
         user_id = result[0]
@@ -139,4 +134,4 @@ def delete_tweet(tweet_num):
         else:
             g.db.execute('DELETE FROM tweet WHERE id=?;', (tweet_num,))
             g.db.commit()
-            return redirect(url_for('index'), code=302)
+            return redirect('/')
