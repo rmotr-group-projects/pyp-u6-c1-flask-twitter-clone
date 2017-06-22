b7b595a64ec512affd8a8ecec9923d39a4fed19e
Santiago Basulto <santiago.basulto@gmail.com>
2017-06-22 20:41:02 -0300

    Update twitter-schema.sql

--- twitter-schema.sql
+++ twitter-schema.sql
@@ -27,10 +27,3 @@ CREATE TABLE tweet (
       length("content") <= 140
   )
 );
-
---INSERT INTO "user" ("id", "username", "password", "birth_date") VALUES (10, "martinzugnoni", "81dc9bdb52d04dc20036dbd8313ed055", "2016-01-26");
---INSERT INTO "user" ("id", "username", "password", "birth_date") VALUES (11, "ivanzugnoni", "81dc9bdb52d04dc20036dbd8313ed055", "2016-07-06");
---INSERT INTO "tweet" ("user_id", "content") VALUES (10, "Hello World!");
---INSERT INTO "tweet" ("user_id", "content") VALUES (10, "This is so awesome");
---INSERT INTO "tweet" ("user_id", "content") VALUES (10, "Testing twitter clone");
---SELECT * FROM tweet;
