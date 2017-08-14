c2de5dd5f2fe0b953ec4408cbc602e9ffe7661bd
Santiago Basulto <santiago.basulto@gmail.com>
2017-08-14 02:13:40 -0300

    Working solution

--- initial-data.sql
+++ initial-data.sql
@@ -1,4 +1,5 @@
 
+-- Password is MD5(1234)
 INSERT INTO "user" ("id", "username", "password", "birth_date") VALUES (10, "martinzugnoni", "81dc9bdb52d04dc20036dbd8313ed055", "2016-01-26");
 INSERT INTO "user" ("id", "username", "password", "birth_date") VALUES (11, "ivanzugnoni", "81dc9bdb52d04dc20036dbd8313ed055", "2016-07-06");
 INSERT INTO "tweet" ("user_id", "content") VALUES (10, "Hello World!");
